5b3ac01d739e767c7892bd3a23158e0ddb04d469
orz <orz@orz.orz>
2022-05-11 19:45:51 +0800

    tool to generate associations.txt

---
+++ generate_associations.py
@@ -0,0 +1,32 @@
+import glob
+import os
+# change this if needed
+data_dir = "/home/orz/Projects/Datasets/dorm2/dorm2_fast"
+depth_name = "filtered"
+color_name = "color"
+image_name = "*.png"                # Pick one of two. Generate by traversing file name
+timestamp_name = "TIMESTAMP.txt"    # Pick one of two. Generate by reading timestamp.txt, see FastFusion datasets https://github.com/zhuzunjie17/FastFusion
+log_name = "associations.txt"
+
+idx=0
+timestamp_path = os.path.join(data_dir, timestamp_name)
+log_path = os.path.join(data_dir, log_name)
+with open(log_path, 'w') as f:
+    if not os.path.exists(timestamp_path):
+        depth_paths = sorted(glob.glob(os.path.join(data_dir, depth_name, image_name)))
+        color_paths = sorted(glob.glob(os.path.join(data_dir, color_name, image_name)))
+
+        for i, (depth_path, color_path) in enumerate(zip(depth_paths, color_paths)):
+            depth_filename = depth_path.split("/")[-1]
+            color_filename = color_path.split("/")[-1]
+            f.write("{} {}/{} {} {}/{}".format(idx, depth_name, depth_filename, idx, color_name, color_filename))
+            idx += 1
+
+    else:
+        with open(timestamp_path, 'r') as ft:
+            infos = ft.readlines()
+            infos.pop(0)
+            for info in infos:
+                _, depth_filename, color_filename = info.split(',')
+                f.write("{} {}/{} {} {}/{}".format(idx, depth_name, depth_filename, idx, color_name, color_filename))
+                idx += 1
